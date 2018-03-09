e6066002828abda233ba61542fa0946036cd9180
as-xjc <azuresky.xjc@gmail.com>
2018-03-09 17:09:38 +0800

    add sample

---
+++ defer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+
+class Defer final {
+ public:
+  Defer() = delete;
+  Defer& operator=(const Defer&) = delete;
+
+  Defer(std::function<void()>&& cb) {
+    cbs_.emplace_back(std::move(cb));
+  }
+  ~Defer() {
+    Done();
+  }
+
+  Defer& Then(std::function<void()>&& cb) {
+    cbs_.emplace_back(std::move(cb));
+    return *this;
+  }
+
+  void Done() {
+    if (cbs_.empty()) return;
+
+    for (auto it = cbs_.rbegin(); it != cbs_.rend(); ++it) {
+      (*it)();
+    }
+    cbs_.clear();
+  }
+
+ private:
+  std::vector<std::function<void()>> cbs_;
+};
+
+/**
+ * use one times in a life scope only
+ */
+#define DEFER(cmd) ::Defer ___simulate_go_defer___([&]() { cmd; })
+
+#define DEFER_IN_CLASS(cmd) ::Defer ___simulate_go_defer_class___([&, this]() { cmd; })
---
+++ sample.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "defer.h"
+
+class Print {
+ public:
+  Print(std::string msg) : msg_(msg) {}
+  ~Print() {
+    std::cout << "message:" << msg_ << std::endl;
+  }
+  std::string msg_;
+};
+
+class test {
+ public:
+  void Do() {
+    int a = 5;
+    int b = 6;
+    std::cout << "a:" << a << std::endl;
+    DEFER(std::cout << "defer b:" << b << std::endl);
+    std::cout << "b:" << b << std::endl;
+    b = 7;
+  }
+  void msg(Print* p) {
+    delete p;
+  }
+  void PrintMsg() {
+    auto p = new Print("print the msg");
+    DEFER_IN_CLASS(msg(p));
+  }
+
+};
+
+int main() {
+
+  DEFER(std::cout << "return hello" << std::endl;);
+  {
+    DEFER(std::cout << "> hello" << std::endl;);
+    {
+      DEFER(std::cout << ">> hello" << std::endl;
+                std::cout << ">> yeah" << std::endl;
+                std::cout << ">> hehe" << std::endl;
+                std::cout << ">> haah" << std::endl;);
+    }
+  }
+  test t;
+  t.Do();
+  t.PrintMsg();
+
+  Defer d([]() {std::cout << "-- im 1" << std::endl; });
+  d.Then([]() {std::cout << "-- im 2" << std::endl; }).
+      Then([]() {std::cout << "-- im 3" << std::endl; }).
+      Then([]() {std::cout << "-- im 4" << std::endl; });
+  return 0;
+}
